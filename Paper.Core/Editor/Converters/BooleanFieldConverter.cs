6e9ce0c13747515b9d686a57f931c246c2bd5397
itsBuggingMe <lohj002@gmail.com>
2026-04-06 19:57:36 -0400

    bool field

---
+++ Paper.Core/Editor/Converters/BooleanFieldConverter.cs
@@ -0,0 +1,13 @@
+﻿using ImGuiNET;
+
+namespace Paper.Core.Editor.Converters;
+
+internal class BoolFieldConverter : FieldModifierBase<bool>
+{
+    protected override bool UpdateValue(ComponentField field)
+    {
+        bool b = _current;
+        ImGui.Checkbox(field.Name, ref b);
+        return b;
+    }
+}
