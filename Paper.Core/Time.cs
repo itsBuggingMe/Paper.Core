669302ff587bb9320ee5cec0502460942a53b19c
itsBuggingMe <lohj002@gmail.com>
2026-07-16 21:18:09 -0400

    added a second time measurement

--- Paper.Core/Time.cs
+++ Paper.Core/Time.cs
@@ -5,13 +5,15 @@ namespace Paper.Core;
 public class Time
 {
     public float FrameDeltaTime { get; private set; }
+    public float DeltaTime { get; private set; }
 
-    public GameTime GameTime { get; private set; }
+    public GameTime GameTime { get; private set; } = null!;
 
 
     public void SetValues(GameTime gameTime)
     {
         FrameDeltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds / (1 / 60f);
+        DeltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
         GameTime = gameTime;
     }
 }
